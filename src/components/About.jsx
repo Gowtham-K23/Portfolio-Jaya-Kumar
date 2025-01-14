4814e1449d6cc2f368921d01ac2989343966da96
Gowtham-K23 <gowthamk2394@gmail.com>
2025-01-14 17:41:46 +0530

    changes made in the file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,14 +18,14 @@ const About = () => {
         <img src = {Hero1Img} alt = "profile" type = "image/png" className = 'w-80 h-96 rounded-xl transform transition-transform duration-300 hover:scale-105' />
         </div>
         
-        <p className='text-center mt-6 px-4 md:px-20 text-xl font-bold font-montserrat text-black'>Hello! I'm Jaya Kumar, a passionate graphic designer, video editor, and social media marketer, blending creativity with technology. With a solid foundation in Computer Science, I craft visually striking designs, produce engaging video content, and curate effective social media strategies. Constantly evolving, I strive to push the limits of design, art, and innovation to deliver impactful, high-quality work</p>
+        <p className='text-center mt-6 px-4 md:px-20 text-xl font-bold font-montserrat text-black'>Hello! I'm Jaya Kumar, a passionate graphic designer, video editor, and social media marketer. I craft visually striking designs, produce engaging video content, and curate effective social media strategies. Constantly evolving, I strive to push the limits of design, art, and innovation to deliver impactful, high-quality work. I am a professional graphic designer specializing in social media, concept-oriented, and product posts. With expertise in Adobe Photoshop, Illustrator, and Premiere Pro, I deliver high-quality, visually impactful designs tailored to client needs.</p>
         
         <div className='grid grid-cols:1 sm:grid-cols-3 lg:grid-cols-3 gap-8 mt-12 px-4 md:px-20'>
 
             <div className = 'flex flex-col items-center bg-pink-200 p-6 rounded-lg shadow-lg hover:shadow-2xl transform transition-all duration-300 hover:scale-105'>
                 <img src = {ExperienceImg} alt = "Experience" type = "image/png" className = 'w-16 h-16 mb-4'/>
                 <h3 className='text-2xl font-semibold font-serif mb-2'>Experience</h3>
-                <p className='text-center text-lg font-semibold text-black font-montserrat'>2+ years of experience in graphic design, video editing, and social media marketing, creating engaging visuals and impactful content</p>
+                <p className='text-center text-lg font-semibold text-black font-montserrat'>2+ years of experience in graphic design and video editing, creating engaging visuals and impactful content</p>
             </div>
 
             <div className = 'flex flex-col items-center bg-pink-200 p-6 rounded-lg shadow-lg hover:shadow-2xl transform transition-all duration-300 hover:scale-105'>
